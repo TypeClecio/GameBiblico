9d4c4336967913e27cfdb7f4cf8c8b73996134a2
TypeClecio <devclecio@gmail.com>
2026-02-16 23:53:19 -0300

    Ajustando TelaInicio finalizando estilização da página

--- src/pages/Inicio/Inicio.tsx
+++ src/pages/Inicio/Inicio.tsx
@@ -8,7 +8,7 @@ export default function Inicio() {
     <main className={styles.escopo}>
       <section id="informacoes" className={styles.secaoInformacoes}>
         <button className={styles.botaoDeInformacoes}>
-          <Info />
+          <Info size={30} strokeWidth={2.5} />
         </button>
       </section>
 
@@ -17,23 +17,23 @@ export default function Inicio() {
           <img src={avatar} alt="Avatar" />
         </div>
 
-        <div id="nome-e-id-jogador">
-          <h3>TochaViva</h3>
-          <p>#469874563</p>
+        <div id="nome-e-id-jogador" className={styles.identificadoresJogador}>
+          <h3 className={styles.nomeJogador}>TochaViva</h3>
+          <p className={styles.idJogador}>#469874563</p>
         </div>
 
-        <button id="iniciar-partida">Iniciar Partida</button>
+        <button id="iniciar-partida" className={styles.botaoIniciarPartida}>Iniciar Partida</button>
       </section>
       
       <section id="guias" className={styles.secaoGuias}>
-        <button id="ranking">
-          <Trophy />
+        <button id="ranking" className={styles.botoesGuias}>
+          <Trophy size={30} strokeWidth={2.5} />
           
           <span>Ranking</span>
         </button>
         
-        <button id="perfil">
-          <UserRoundPen />
+        <button id="perfil" className={styles.botoesGuias}>
+          <UserRoundPen size={30} strokeWidth={2.5} />
           
           <span>Pefil</span>
         </button>
