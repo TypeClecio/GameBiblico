855da7f0a700d28aad8ed1ca9d4e3c525b45bb9f
TypeClecio <devclecio@gmail.com>
2026-02-16 23:58:15 -0300

    Ajustando Rotas no index da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 
 import "./assets/styles/global.scss";
@@ -8,7 +8,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/*" element={<Navigate to="/" replace />} />
           <Route path="/" element={<>Início</>} />
+          <Route path="/inicio" element={<>Início</>} />
         </Routes>
       </BrowserRouter>
 
