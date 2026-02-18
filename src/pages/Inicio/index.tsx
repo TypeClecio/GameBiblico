c6fd73e79f192edea63e0b69ec689841520c5093
TypeClecio <devclecio@gmail.com>
2026-02-17 23:40:28 -0300

    Ajustando TelaInicial finalizando o estilo e estrutura

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -6,35 +6,35 @@ import avatar from "../../../public/avatar-1-gamebiblico.svg";
 export default function Inicio() {
   return (
     <main className={styles.escopo}>
-      <section id="informacoes" className={styles.secaoInformacoes}>
+      <section className={styles.secaoInformacoes}>
         <button className={styles.botaoDeInformacoes}>
-          <Info size={30} strokeWidth={2.5} />
+          <Info size={22} strokeWidth={3} />
         </button>
       </section>
 
-      <section id="avatar" className={styles.secaoAvatar}>
-        <div id="imagem-jogador" className={styles.imagemJogador}>
+      <section className={styles.secaoJogador}>
+        <div className={styles.imagemJogador}>
           <img src={avatar} alt="Avatar" />
         </div>
 
-        <div id="nome-e-id-jogador" className={styles.identificadoresJogador}>
+        <div className={styles.identificadoresJogador}>
           <h3 className={styles.nomeJogador}>TochaViva</h3>
           <p className={styles.idJogador}>#469874563</p>
         </div>
 
-        <button id="iniciar-partida" className={styles.botaoIniciarPartida}>Iniciar Partida</button>
+        <button className={styles.botaoIniciarPartida}>Iniciar Partida</button>
       </section>
-      
-      <section id="guias" className={styles.secaoGuias}>
-        <button id="ranking" className={styles.botoesGuias}>
-          <Trophy size={30} strokeWidth={2.5} />
-          
+
+      <section className={styles.secaoGuias}>
+        <button className={styles.botoesGuias}>
+          <Trophy size={25} strokeWidth={2.5} />
+
           <span>Ranking</span>
         </button>
-        
-        <button id="perfil" className={styles.botoesGuias}>
-          <UserRoundPen size={30} strokeWidth={2.5} />
-          
+
+        <button className={styles.botoesGuias}>
+          <UserRoundPen size={25} strokeWidth={2.5} />
+
           <span>Pefil</span>
         </button>
       </section>
