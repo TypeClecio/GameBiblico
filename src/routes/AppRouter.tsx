51a2dd6ea130e31022c2589188c92a2ce9fd65cc
TypeClecio <devclecio@gmail.com>
2026-02-22 00:57:27 -0300

    Commit inicial

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
+import Ranking from '../pages/Ranking';
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/*" element={<Navigate to="/" replace />} />
-      <Route path="/" element={<>Página inicial</>} />
+      <Route path="/ranking" element={<Ranking />} />
     </Routes>
   )
 };
