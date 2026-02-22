bd010fba1cc8429ae22028c6b54acd744a3ad3e1
TypeClecio <devclecio@gmail.com>
2026-02-22 00:20:42 -0300

    Resetando estrutura para melhor adequação em novas branchs

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,15 +1,10 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-import Inicio from '../pages/Inicio';
-import Perfil from '../pages/Perfil';
-
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/*" element={<Navigate to="/" replace />} />
-      <Route path="/" element={<Inicio />} />
-      <Route path="/inicio" element={<Inicio />} />
-      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/" element={<>Página inicial</>} />
     </Routes>
   )
 };
