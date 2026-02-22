0d126301a00fbd74f32b5609c5264ed7d28d6ed8
TypeClecio <devclecio@gmail.com>
2026-02-22 01:19:30 -0300

    Commit inicial

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 
+import Jogatina from '../pages/Jogatina';
+
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/*" element={<Navigate to="/" replace />} />
-      <Route path="/" element={<>Página inicial</>} />
+      <Route path="/jogatina" element={<Jogatina />} />
     </Routes>
   )
 };
