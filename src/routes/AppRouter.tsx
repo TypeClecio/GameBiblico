dd986e450ea35bd91255b12c6a0d9cc29f54d6a7
TypeClecio <devclecio@gmail.com>
2026-02-23 22:35:19 -0300

    Ajustando AppRouter para não listar rotas a não ser Ranking

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,11 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-import Inicio from '../pages/Inicio';
-import Perfil from '../pages/Perfil';
 import Ranking from '../pages/Ranking';
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/*" element={<Navigate to="/" replace />} />
-      <Route path="/" element={<Inicio />} />
-      <Route path="/inicio" element={<Inicio />} />
-      <Route path="/perfil" element={<Perfil />} />
       <Route path="/ranking" element={<Ranking />} />
     </Routes>
   )
