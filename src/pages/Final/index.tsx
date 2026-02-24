5bd47ea0fd4005ff10a35e60a0ad1144c1714ea2
TypeClecio <devclecio@gmail.com>
2026-02-24 01:18:25 -0300

    Restaura páginas Perfil e Inicio a partir das branchs respectivas

--- src/pages/Final/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Final.module.scss";
-
-export default function Final() {
-  return (
-    <main className={styles.escopo}>Página Final</main>
-  )
-}
--- src/pages/Jogatina/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Jogatina.module.scss";
-
-export default function Jogatina() {
-  return (
-    <main className={styles.escopo}>Página Jogatina</main>
-  )
-}
