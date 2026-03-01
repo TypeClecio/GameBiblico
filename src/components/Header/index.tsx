b0ad390a96d39d0a358ac9e1fd2ca67a11556c3a
TypeClecio <devclecio@gmail.com>
2026-03-01 15:04:28 -0300

    Configurando o Componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,29 @@
-export default function Cabecalho() {
+import { ChevronLeft } from "lucide-react";
+
+import styles from "./Header.module.scss";
+
+interface CabecalhoProps {
+  tituloPagina?: string;
+  link?: string;
+  className?: string;
+}
+
+export default function Cabecalho({
+  tituloPagina,
+  link = "/",
+  className = ""
+}: CabecalhoProps) {
   return (
-    <></>
+    <section className={`${styles.secaoCabecalho} ${className}`}>
+      <a href={link}>
+        <button>
+          <ChevronLeft size={22} strokeWidth={3} />
+        </button>
+      </a>
+
+      <h6 className={styles.tituloPagina}>{tituloPagina}</h6>
+
+      <div className={`${styles.recursoExtra} ${styles.desativado}`}>.</div>
+    </section>
   );
 };
