0cc1d18a49e6dab06a6595785eae3ff639ef7729
TypeClecio <devclecio@gmail.com>
2026-03-03 22:00:46 -0300

    Ajustando o Componente Avatar para o tamanho ser ajustado de acordo com o parametro

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,21 +3,24 @@ import styles from "./Avatar.module.scss";
 interface AvatarProps {
   src?: string;
   alt?: string;
+  tamanho?: number;
 }
 
 /**
  * 
  * @param src Indica o caminho do Ícone
  * @param alt Texto alternativo caso não carregue o Ícone
+ * @param tamanho Define a largura e altura do Ícone
  * @description Componente renderizando o Avatar do jogador
  */
 export default function Avatar({
   src = "",
-  alt = "Avatar"
+  alt = "Avatar",
+  tamanho = 50
 }: AvatarProps) {
   return (
     <div className={styles.imagemJogador}>
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} width={tamanho} height={tamanho} />
     </div>
   )
 }
