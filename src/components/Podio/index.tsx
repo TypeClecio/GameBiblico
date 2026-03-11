9e62a61d32d2d954b0e7c3b70ec659e93ac0110b
TypeClecio <devclecio@gmail.com>
2026-03-10 22:27:45 -0300

    Modificando a estrutura do Podio

--- src/components/Podio/index.tsx
+++ src/components/Podio/index.tsx
@@ -1,5 +1,4 @@
-import type { Jogador } from "../../interfaces/Jogador";
-
+import { ordemPodio, podio } from "../../data/podio";
 import styles from "./Podio.module.scss";
 
 import Avatar from "../Avatar";
@@ -9,37 +8,11 @@ interface PodioProps {
   estilo?: string;
 }
 
-const jogadores: Jogador[] = [
-  {
-    nome: "TochaViva",
-    id: "469874563",
-    tempo: 49.8,
-    avatar: 0,
-    criado_em: "2026-03-03T17:15:02.539Z",
-  },
-  {
-    nome: "Jurema",
-    id: "758991532",
-    tempo: 50.2,
-    avatar: 1,
-    criado_em: "2026-03-03T17:15:02.539Z",
-  },
-  {
-    nome: "Tocha",
-    id: "132756984",
-    tempo: 54.4,
-    avatar: 2,
-    criado_em: "2026-03-03T17:15:02.539Z",
-  },
-];
-
-const ordemPodio = [1, 0, 2];
-
 export default function Podio({ estilo }: PodioProps) {
   return (
     <section className={`${styles.secaoPodio} ${estilo}`}>
       {ordemPodio.map((indiceJogador) => {
-        const jogador = jogadores[indiceJogador];
+        const jogador = podio[indiceJogador];
         const posicao = indiceJogador + 1;
 
         return (
---
+++ src/data/podio.ts
@@ -0,0 +1,27 @@
+import type { Jogador } from "../interfaces/Jogador";
+
+export const ordemPodio = [1, 0, 2];
+
+export const podio: Jogador[] = [
+  {
+    nome: "TochaViva",
+    id: "469874563",
+    tempo: 49.8,
+    avatar: 0,
+    criado_em: "2026-03-03T17:15:02.539Z",
+  },
+  {
+    nome: "Jurema",
+    id: "758991532",
+    tempo: 50.2,
+    avatar: 1,
+    criado_em: "2026-03-03T17:15:02.539Z",
+  },
+  {
+    nome: "Tocha",
+    id: "132756984",
+    tempo: 54.4,
+    avatar: 2,
+    criado_em: "2026-03-03T17:15:02.539Z",
+  },
+];
